297aa26314d9c4103fd7b5e2713162131c22ad7f
1053190902@qq.com <1053190902@qq.com>
2018-05-17 19:01:45 +0800

    FINALLY

--- CaicoWeb/javascript/control.js
+++ CaicoWeb/javascript/control.js
@@ -12,7 +12,47 @@
             }
         }
         */
-       var imgs = [
+       var imgs = 
+       [
+        '/font/小米兰亭字体.ttf',
+        'logo.png',
+        'Double Ring-1s-200px.svg',
+        '/image/CN_sina_weibo.svg',
+        '/image/Popular_Facebook_.svg',
+        '/image/Popular_G+.svg',
+        '/image/Popular_GitHub.svg',
+        '/image/Popular_Twitter.svg',
+        '/image/Popular_mail.svg',
+        '/image/back.png',
+        '/image/forward.png',
+        '/image/head.png',
+        '/image/myWechat.jpg',
+        '/image/pic1.jpg',
+        '/image/pic2.jpg',
+        '/image/pic3.jpg',
+        '/image/pic4.jpg',
+        '/image/pic5.jpg',
+        '/image/pic6.jpg',
+        '/image/pic7.jpg',
+        '/image/对.png',
+        '/image/灯泡.png',
+        '/image/钻石.png',
+        '/image/list1.jpg',
+        '/image/list2.jpg',
+        '/image/list3.jpg',
+        '/image/list4.jpg',
+        '/image/list5.jpg',
+        '/image/list6.jpg',
+        '/image/list7.jpg',
+        '/image/list8.jpg',
+        '/image/list9.jpg',
+        '/image/25_de_abril_bridge_night_tagus_river_portugal-wallpaper-2560x1080.jpg',
+        '/image/beautiful_city-wallpaper-1366x768.jpg',
+        '/image/download.png',
+        '/image/park_8-wallpaper-1920x1080.jpg'
+    ],
+       /*
+       [
         'https://caicoleung.github.io/CaicoWeb/logo.png',
         'https://caicoleung.github.io/CaicoWeb/Double Ring-1s-200px.svg',
         'https://caicoleung.github.io/CaicoWeb/image/CN_sina_weibo.svg',
@@ -44,8 +84,13 @@
         'https://caicoleung.github.io/CaicoWeb/image/list7.jpg',
         'https://caicoleung.github.io/CaicoWeb/image/list8.jpg',
         'https://caicoleung.github.io/CaicoWeb/image/list9.jpg',
-        'https://caicoleung.github.io/CaicoWeb/image/25_de_abril_bridge_night_tagus_river_portugal-wallpaper-2560x1080.jpg'
+        'https://caicoleung.github.io/CaicoWeb/image/25_de_abril_bridge_night_tagus_river_portugal-wallpaper-2560x1080.jpg',
+        'https://caicoleung.github.io/CaicoWeb/font/小米兰亭字体.ttf',
+        'https://caicoleung.github.io/CaicoWeb/image/beautiful_city-wallpaper-1366x768.jpg',
+        'https://caicoleung.github.io/CaicoWeb/image/download.png',
+        'https://caicoleung.github.io/CaicoWeb/image/park_8-wallpaper-1920x1080.jpg',
     ],
+    */
         $progress = $(".progress-bar"),
         $progressVal = $(".progress-value"),
         len = imgs.length,
